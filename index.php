b224f95a0fcb952837beeca281c93645ae31de71
Caacaaaa <yuriskazsalsabilla@gmail.com>
2026-05-02 20:47:23 +0700

    kuisweb

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama_toko = "Warung Digital Unila";
+
+function hitungTotal($harga) {
+    global $nama_toko;
+    echo "<h1>Selamat Datang di $nama_toko</h1><br>";
+
+    static $jumlah_transaksi = 0;
+    $jumlah_transaksi++;
+
+    echo "Harga Barang : Rp $harga <br>";
+    echo "Transaksi ke : $jumlah_transaksi <br>";
+    echo "<hr>";
+    
+}
+
+hitungTotal(15000);
+hitungTotal(25000);
+hitungTotal(50000);
+
+?>
